92dc969f778fe8f2e79a5e085b6ea2c078b84d4f
AbeerFatima238 <abeerfatimahom2@gmail.com>
2024-11-01 14:27:59 +0500

    first commit

---
+++ app/thank-you/page.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link"
+export default function PlaceYourOrder(){
+    return(
+        <div className="font-extrabold text-center text-6xl mt-44 font-serif text-purple-500">
+            Thank You For Shopping !!!
+        </div>
+    )
+} 
